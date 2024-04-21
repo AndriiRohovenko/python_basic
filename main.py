0399e277d2d10468449ef4a44ae85f0f06cf7494
Andrii Rohovenko <114521899+AndriiRohovenko@users.noreply.github.com>
2024-04-21 21:03:42 +0300

    Add Class WoWo with method to print Greetigs msg

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-a = "hello world!!!!!"
-b = "hello git!!!"
-c = "Carpe diem!!"
-for i in range(1, 100):
-    print(f"wowoowo {a} and {b}\n{c}")
+class WoWo():
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+    def print_smg(self, count:int=1):
+        try:
+            a = "hello world!!!!!"
+            b = "hello git!!!"
+            c = "Carpe diem!!"
+            for _ in range(count):
+                print(f"wowoowo {a} and {b}\n{c}\nFrom: {self.name} > {self.age} years old.")
+        except Exception as ex:
+            print(f"An error has occured: {ex}")
+
+
+
+a = WoWo("Andrii", 29)
+
+a.print_smg(count=100)

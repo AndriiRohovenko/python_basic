d07215731efe1323e245a8fecf3fca44c8711327
Andrii Rohovenko <114521899+AndriiRohovenko@users.noreply.github.com>
2024-07-13 21:36:01 +0300

    removed un-needed print from the hw_3 > task3

--- goit-pycore-hw-03/task_3.py
+++ goit-pycore-hw-03/task_3.py
@@ -17,7 +17,7 @@ def normalize_phone(phone_number):
             else:
                 # Otherwise, add '+38' at the beginning
                 phone_number = '+38' + phone_number
-        
+        print(phone_number)
         return phone_number
     except Exception as ex:
         print(f"An unknown error has occurred! {ex}")

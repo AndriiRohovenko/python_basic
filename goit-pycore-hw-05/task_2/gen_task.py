5da2e2e591cdf875d69d9055493a76312817bd2f
Andrii Rohovenko <114521899+AndriiRohovenko@users.noreply.github.com>
2024-07-21 16:09:13 +0300

    Add HW5 > task 2

---
+++ goit-pycore-hw-05/task_2/gen_task.py
@@ -0,0 +1,19 @@
+import re
+from collections.abc import Generator
+from typing import Callable
+
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+
+    # Regular expression to find valid numbers in the text
+    pattern = ...
+    matches = re.findall(pattern, text)
+    for match in matches:
+        yield float(match)
+
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]):
+    # Calculates the total profit
+    # by summing up all the numbers yielded by the generator function.
+    total_sum = sum(func(text))
+    return total_sum
